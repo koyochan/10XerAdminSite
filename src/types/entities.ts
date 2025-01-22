d963202b100396c80547656d37682f278d435d76
koyochan <121277745+koyochan@users.noreply.github.com>
2025-01-22 14:38:24 +0900

    embed AchievementSheets

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -15,6 +15,32 @@ export interface Product {
 	discount: string;
 }
 
+export const skills: string[] = [
+  "集中力",
+  "創造性",
+  "習得度",
+  "コミュニケーション能力",
+  "問題解決能力",
+];
+
+export interface Achievement {
+  created_at: Date;
+  updated_at: Date;
+  student_name: string;
+  date: string;
+//   teacher: string;
+//   activity: string;
+//   goal: string;
+//   progress: string;
+//   progress_percentage: number;
+  ratings: number[];
+  xp_earned: number;
+//   teacher_comment: string;
+  start_time: number;
+  end_time: number;
+  duration: number;
+}
+
 export type Users = User[];
 export interface User {
 	id: number;
@@ -26,5 +52,6 @@ export interface User {
 	country: string;
 	status: string;
 }
+
 
 export type PlaygroundAction = (typeof playgroundActions)[number];
